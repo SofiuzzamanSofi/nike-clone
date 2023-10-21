bd1b831850274883ee85d0a01e70c1d8e677fc48
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-10-21 15:06:07 +0600

    setup app in nextjs

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+interface NavbarProps {
+
+};
+
+const Navbar: FC<NavbarProps> = ({ }) => {
+    return (
+        <div>
+            Navbar
+        </div>
+    );
+};
+
+export default Navbar;
