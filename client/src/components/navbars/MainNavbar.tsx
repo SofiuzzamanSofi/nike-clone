da51636b6cd0ed7df259d58d86f21f9f6cb69e75
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-10-21 21:11:07 +0600

    svg not work

--- client/src/components/navbars/MainNavbar.tsx
+++ client/src/components/navbars/MainNavbar.tsx
@@ -1,4 +1,8 @@
 import { FC } from 'react';
+import Image from "next/image"
+import nikeNavbarLogo from "../../assets/navbars/nike-navbar-logo.svg"
+import favoritesLogo from "../../assets/navbars/favorites-logo.svg"
+import cartLogo from "../../assets/navbars/cart-logo.svg"
 
 interface MainNavbarProps {
 
@@ -7,9 +11,31 @@ interface MainNavbarProps {
 const MainNavbar: FC<MainNavbarProps> = ({ }) => {
     return (
         <div>
-            MainNavbar
+            <div>
+                <button>
+                    <Image src={nikeNavbarLogo} className='h-full w-full' alt='nike-navbar-logo-image' />
+                </button>
+            </div>
+            <div>
+                <div>
+                    <button>New & Featured</button>
+                    <button>Men</button>
+                    <button>Women</button>
+                    <button>Kids</button>
+                </div>
+                <div>
+                    <div>
+                        Search Bar
+                    </div>
+                    <button>
+                        <Image src={favoritesLogo} className='h-full w-full' alt='favorites-logo-image' />
+                    </button>
+                    <button>
+                        <Image src={cartLogo} className='h-full w-full' alt='cart-logo-image' />
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
-
 export default MainNavbar;
--- client/src/components/navbars/UpMiniNavbar.tsx
+++ client/src/components/navbars/UpMiniNavbar.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import rightArrow from "../../assets/navbars/right-arrow.svg"
+import menWithHandBall from "../../assets/navbars/men-with-hand-ball.svg"
 import { FC } from 'react';
 
 interface UpMiniNavbarProps {
@@ -7,7 +10,21 @@ interface UpMiniNavbarProps {
 const UpMiniNavbar: FC<UpMiniNavbarProps> = ({ }) => {
     return (
         <div>
-            UpMiniNavbar
+            <div>
+                <button>
+                    <Image src={rightArrow} className='h-full w-full' alt='right-arrow-image' />
+                </button>
+                <button>
+                    <Image src={menWithHandBall} className='h-full w-full' alt='men-with-hand-ball-image' />
+                </button>
+            </div>
+            <div>
+                <button>Nike Refurbished</button>
+                <button>Find a Store</button>
+                <button>Help</button>
+                <button>Join Us</button>
+                <button>Sign In</button>
+            </div>
         </div>
     );
 };
