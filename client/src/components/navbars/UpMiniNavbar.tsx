af2c6959a3da3ad00c5a4f6a1c8842d534856610
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-10-22 14:24:41 +0600

    tailwind work on all component under src

--- client/src/components/navbars/UpMiniNavbar.tsx
+++ client/src/components/navbars/UpMiniNavbar.tsx
@@ -11,20 +11,22 @@ interface UpMiniNavbarProps {
 const UpMiniNavbar: FC<UpMiniNavbarProps> = ({ }) => {
     return (
         <div>
-            <div>
-                <button>
-                    <Image src={rightArrow} className='h-full w-full' alt='right-arrow-image' />
-                </button>
-                <button>
-                    <Image src={menWithHandBall} className='h-full w-full' alt='men-with-hand-ball-image' />
-                </button>
-            </div>
-            <div>
-                <button>Nike Refurbished</button>
-                <button>Find a Store</button>
-                <button>Help</button>
-                <button>Join Us</button>
-                <button>Sign In</button>
+            <div className="max-w-[120rem] mx-auto border">
+                <div>
+                    <button>
+                        <Image src={rightArrow} className='h-full w-full' alt='right-arrow-image' />
+                    </button>
+                    <button>
+                        <Image src={menWithHandBall} className='h-full w-full' alt='men-with-hand-ball-image' />
+                    </button>
+                </div>
+                <div>
+                    <button className="bg-red-700 text-white">Nike Refurbished</button>
+                    <button>Find a Store</button>
+                    <button>Help</button>
+                    <button>Join Us</button>
+                    <button>Sign In</button>
+                </div>
             </div>
         </div>
     );
