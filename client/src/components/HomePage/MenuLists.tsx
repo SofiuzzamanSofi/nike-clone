4300fdc1e3fb85d9b49db447139c3ebfe0542f93
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-10-22 10:12:09 +0600

    menu list added without css

--- client/src/components/HomePage/MenuLists.tsx
+++ client/src/components/HomePage/MenuLists.tsx
@@ -1,30 +1,97 @@
 import { FC } from 'react';
-import Image from 'next/image';
-import featured1 from "../../assets/homePages/featured/fetured-1.jpeg"
-import featured2 from "../../assets/homePages/featured/fetured-2.jpeg"
+import Link from 'next/link';
 
 
 const MenuLists = ({ }) => {
+
+    const featuredLink = [
+        "Air Force 1",
+        " Jordan 1",
+        "    Metcon",
+        " Air Max 270",
+        "  Air Max 97",
+        "   Air Max 90",
+        "    Blazer",
+        "  Pegasus",
+    ];
+    const shoesLink = [
+        "    All Shoes",
+        " Jordan Shoes",
+        " Running Shoes",
+        " Basketball Shoes",
+        "  Tennis Shoes",
+        "  Training Shoes",
+        "   Custom Shoes",
+        " Sale Shoes",
+        "  Soccer Cleats",
+    ];
+    const clothingLink = [
+        " All Clothing",
+        " Tops & T-Shirts",
+        " Shorts",
+        " Hoodies & Pullovers",
+        " Joggers & Sweatpants",
+        "  Sports Bras",
+        " Pants & Tights",
+        "   Socks",
+        "  Yoga",
+        " NikeLab",
+        "  Plus Size",
+        "  Big & Tall",
+        "Sale Clothing",
+    ];
+    const kidsLink = [
+        " Infant & Toddler Shoes",
+        "Kids Shoes",
+        "Kids Basketball Shoes",
+        " Kids Running Shoes",
+        " Kids Jordan Shoes",
+        "  Kids Clothing",
+        "  Kids Backpacks",
+        " Kids Socks",
+        " Kids Sale",
+    ];
+
     return (
         <div>
             <div>
                 <div>
                     <p>Featured</p>
+                    {
+                        featuredLink.map((link, index) => (
+                            <Link key={index} href={""} >{link}</Link>
+                        ))
+                    }
                 </div>
             </div>
             <div>
                 <div>
                     <p>Shoes</p>
+                    {
+                        shoesLink.map((link, index) => (
+                            <Link key={index} href={""} >{link}</Link>
+                        ))
+                    }
                 </div>
             </div>
             <div>
                 <div>
                     <p>Clothing</p>
+                    {
+                        clothingLink.map((link, index) => (
+                            <Link key={index} href={""} >{link}</Link>
+                        ))
+                    }
                 </div>
             </div>
             <div>
                 <div>
                     <p>Kids</p>
+                    {
+                        kidsLink.map((link, index) => (
+                            <Link key={index} href={""} >{link}</Link>
+                        ))
+                    }
                 </div>
             </div>
         </div>
