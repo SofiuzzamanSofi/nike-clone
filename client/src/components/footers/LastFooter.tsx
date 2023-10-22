d92a202513ba08057f3f865ec90aa9a8d7cf4461
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-10-22 10:35:47 +0600

    last two footer with hover

--- client/src/components/footers/LastFooter.tsx
+++ client/src/components/footers/LastFooter.tsx
@@ -5,9 +5,19 @@ interface LastFooterProps {
 };
 
 const LastFooter: FC<LastFooterProps> = ({ }) => {
+
+    const year = new Date().getFullYear();
+
+
     return (
         <div>
-            LastFooter
+            <div>
+                <p><span>location icon</span><span>United States</span></p>
+                <p><span>© {year} Nike, </span><span>Inc. All Rights Reserved</span></p>
+            </div>
+            <div>
+                <p><span>CA </span><span>Supply Chains Act</span></p>
+            </div>
         </div>
     );
 };
--- client/src/components/footers/PrivacyFooter.tsx
+++ client/src/components/footers/PrivacyFooter.tsx
@@ -1,13 +1,44 @@
 import { FC } from 'react';
+import Image from 'next/image';
+import footerTwoColor from "../../assets/footer-two-color.png"
 
 interface PrivacyFooterProps {
 
 };
 
 const PrivacyFooter: FC<PrivacyFooterProps> = ({ }) => {
+
+    const guidesHoverLinks = [
+        " Nike Adapt",
+        " Nike Air",
+        " Nike Air Force 1",
+        " Nike Air Max",
+        " Nike FlyEase",
+        " Nike Flyknit",
+        " Nike Flyleather",
+        " Nike Free",
+        " Nike Joyride",
+        " Nike React",
+        " Nike Vaporfly",
+        " Nike ZoomX",
+        " Space Hippie",
+    ];
+
+    // {
+    //     guidesHoverLinks.map((link, index) => (
+    //         <Link key={index} href={""} >{link}</Link>
+    //     ))
+    // }
+
     return (
         <div>
-            PrivacyFooter
+            <div>
+                <p><span>Guides</span></p>
+                <p><span>Terms of Sale</span></p>
+                <p><span>Terms of Use</span></p>
+                <p><span>Nike Privacy Policy</span></p>
+                <p><span><Image src={footerTwoColor} alt='footer-two-color-image'></Image> </span><span>Your Privacy ChoicesYour Privacy Choices</span></p>
+            </div>
         </div>
     );
 };
